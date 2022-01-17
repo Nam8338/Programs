0176bad46c252292594a9093d204601940b54d4b
Nam8338 <namnguyenquoc911@gmail.com>
2022-01-17 09:18:48 +0700

    Add WebAPI.

---
+++ Web API/Web API/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Web_API.Models
+{
+    [Table("Student")]
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [MaxLength(50)]
+        [MinLength(3)]
+        public string FullName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        public bool Sex { get; set; }
+        public string NativeVillage { get; set; }
+        [MaxLength(10)]
+        [MinLength(7)]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Mail { get; set; }
+    }
+}
